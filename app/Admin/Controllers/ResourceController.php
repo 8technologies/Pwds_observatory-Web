e5f4fe2b287854d4c8687cd885cf5159709ada58
Dali Hillary <dalirichardh@gmail.com>
2023-06-14 10:58:32 +0300

    resources & publications

--- app/Admin/Controllers/ResourceController.php
+++ app/Admin/Controllers/ResourceController.php
@@ -25,16 +25,31 @@ class ResourceController extends AdminController
     protected function grid()
     {
         $grid = new Grid(new Resource());
+        $grid->model()->latest();
+        $grid->quickSearch('title')->placeholder('Search by Title');
+        $grid->filter(function ($f) {
+            // Remove the default id filter
+            $f->disableIdFilter();
+            $f->between('date_of_publication', 'Filter by Date of publication')->date();
+            $f->where( function ($query) {
+                $query->where('type', 'like', "%{$this->input}%")
+                    ->orWhere('other_type', 'like', "%{$this->input}%");
+            }, 'Filter by Type');
+            $f->where( function ($query) {
+                $query->where('author', 'like', "%{$this->input}%");
+            }, 'Filter by Author');
+        });
 
-        $grid->column('id', __('Id'));
         $grid->column('title', __('Title'));
-        $grid->column('type', __('Type'));
-        $grid->column('other_type', __('Other type'));
+        $grid->column('type', __('Type'))->display(function ($type) {
+            if ($this->other_type) {
+                return $this->other_type;
+            }
+            return $type;
+        });
         $grid->column('date_of_publication', __('Date of publication'));
         $grid->column('author', __('Author'));
-        $grid->column('created_at', __('Created at'));
-        $grid->column('updated_at', __('Updated at'));
-
+        
         return $grid;
     }
 
@@ -51,7 +66,7 @@ class ResourceController extends AdminController
         $show->field('id', __('Id'));
         $show->field('title', __('Title'));
         $show->field('type', __('Type'));
-        $show->field('other_type', __('Other type'));
+        // $show->field('other_type', __('Other type'));
         $show->field('date_of_publication', __('Date of publication'));
         $show->field('description', __('Description'));
         $show->field('author', __('Author'));
@@ -71,14 +86,19 @@ class ResourceController extends AdminController
     {
         $form = new Form(new Resource());
 
-        $form->text('title', __('Title'));
-        $form->text('type', __('Type'));
-        $form->text('other_type', __('Other type'));
+        $form->text('title', __('Title'))->rules('required');
+        $form->select('type', __('Type'))->options(['multimedia'=>'Multimedia', 'publication'=>'Publication', 'other'=>'Other','dataset'=>'Dataset'])
+        ->when('=','other', function (Form $form)  {
+            $form->text('other_type', __('Other type'));
+        })->rules('required');
+        // $form->text('other_type', __('Other type'));
         $form->date('date_of_publication', __('Date of publication'))->default(date('Y-m-d'));
         $form->text('author', __('Author'));
-        $form->quill('description', __('Description'));
+        $form->image('cover_photo', __('Cover photo'));
+        $form->quill('description', __('Description'))->rules('required');
 
-        $form->multipleFile('attachments', __('Attachments'));
+        $form->multipleFile('attachments', __('Attachments'))
+        ->help("You can also attach muliple files");
 
         return $form;
     }
