892c03726888b03d32cbeef1b063e0b14f7e3659
Muhindo Mubaraka <mubs0x@gmail.com>
2023-09-06 10:36:27 +0300

    transactionTime

--- app/Http/Controllers/USSDController.php
+++ app/Http/Controllers/USSDController.php
@@ -11,24 +11,50 @@ class USSDController extends Controller
     /* 
     {"post":[],
     "get":{
-        "ussdServiceCode":"255",
-        "msisdn":"256783204665",
-        "transactionTime":"20230906T00:05:48",
-        "transactionId":"16939839342592573",
+        "":"255",
+        "":"256783204665",
+        "":"20230906T00:05:48",
+        "":"16939839342592573",
         "response":"true",
-        "ussdRequestString":"3"
+        "":"3"
     }}
     */
-    public function index(Request $r)
+    public function index()
     {
-        $input = $r->all();
+
         $ussd = new USSD();
         $info['post'] = $_POST;
         $info['get'] = $_GET;
         $ussd->data = json_encode($info);
         $ussd->save();
 
-        $data = "=>".$r->transactionId."\n";
+        $transactionId = "";
+        $ussdServiceCode = "";
+        $transactionId = "";
+        $msisdn = "";
+        $transactionTime = "";
+        $ussdRequestString = "";
+        if (isset($_GET['transactionId'])) {
+            $transactionId = $_GET['transactionId'];
+        }
+        if (isset($_GET['ussdServiceCode'])) {
+            $ussdServiceCode = $_GET['ussdServiceCode'];
+        }
+        if (isset($_GET['msisdn'])) {
+            $msisdn = $_GET['msisdn'];
+        }
+        if (isset($_GET['transactionTime'])) {
+            $transactionTime = $_GET['transactionTime'];
+        }
+        if (isset($_GET['transactionId'])) {
+            $transactionId = $_GET['transactionId'];
+        }
+        if (isset($_GET['ussdRequestString'])) {
+            $ussdRequestString = $_GET['ussdRequestString'];
+        }
+
+
+        $data = "=>" . $transactionId . "\n";
         $data .= "1. Register Person with Disability\n";
         $data .= "2. Request for help\n";
         $data .= "3. Gudance and Canceling\n";
@@ -43,8 +69,8 @@ class USSDController extends Controller
         header('Content-Type: application/xml');
         die('<?xml version="1.0"?>
         <USSDResponse>' .
-            '<TransactionId>' . $r->transactionId . '</TransactionId>' .
-            '<TransactionTime>' . $r->transactionTime . '</TransactionTime>' .
+            '<TransactionId>' . $transactionId . '</TransactionId>' .
+            '<TransactionTime>' . $transactionTime . '</TransactionTime>' .
             '<USSDResponseString>' . $data . '</USSDResponseString>' .
             '<USSDAction>' . $action . '</USSDAction>' .
             '</USSDResponse>');
