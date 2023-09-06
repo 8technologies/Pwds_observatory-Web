e532e79a4ef10c431b198c0bcc6e89e997b39047
Muhindo Mubaraka <mubs0x@gmail.com>
2023-09-06 10:01:34 +0300

    as

--- app/Http/Controllers/USSDController.php
+++ app/Http/Controllers/USSDController.php
@@ -24,7 +24,7 @@ class USSDController extends Controller
         $data .= "6. Jobs\n";
         $data .= "7. Shop\n";
         $data .= "8. Service Providers\n";
-        $action = "<USSDAction></USSDAction>"; 
+        $action = "<USSDAction>end</USSDAction>"; 
 
         header('Content-Type: application/xml');
         die('<?xml version="1.0"?>
